4683a35fd499379582c99a7746259f0cf7365611
unknown <adijuliartha@gmail.com>
2023-05-02 17:50:32 +0800

    setup sass file, asset image, tambah table di wp_editor dan plugin setting website info

--- wp-content/themes/pondoklensa/functions.php
+++ wp-content/themes/pondoklensa/functions.php
@@ -1,2 +1,14 @@
 <?php 
+add_action( 'wp_enqueue_scripts', 'theme_name_scripts' );
+function theme_name_scripts() {
+	$v = '1.0.0'.time();
+
+    wp_enqueue_style( 'style', get_stylesheet_uri() , array(), $v );
+
+    $dt = array();
+	$dt['url'] = admin_url('admin-ajax.php');
+	$dt['please_wait'] = pll__('Please wait');
+
+	wp_localize_script('script-v','d',$dt);
+}
 ?>
